a5ce612b164640176c9d961768840f1097c26f11
Guten <ywzhaifei@gmail.com>
2011-07-27 10:46:39 +0800

    bump to v2.0.3

--- lib/o/version.rb
+++ lib/o/version.rb
@@ -2,8 +2,8 @@ class O
   module VERSION
     MAJOR = 2
     MINOR = 0
-    PATCH = 2
-    PRE = '' # beta rc1
+    PATCH = 3
+    PRE = ''
     IS = [MAJOR, MINOR, PATCH].join(".") + PRE
   end
 end
